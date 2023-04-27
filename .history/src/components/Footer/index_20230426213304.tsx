799d0e5daad45a4c17eb8653d7eb11f13ac7926c
Gabriel <weltergab@gmail.com>
2023-04-27 14:39:18 -0300

    footer

---
+++ .history/src/components/Footer/index_20230426213304.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <>
+      <div className="bg-black w-full p-1 mt-7 rounded">
+        <div className="text-center">
+          <h2 className="font-bold"></h2>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ .history/src/components/Footer/index_20230426213355.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <>
+      <div className="bg-black w-full p-8 mt-7 rounded">
+        <div className="text-center">
+          
+        </div>
+      </div>
+    </>
+  );
+}
